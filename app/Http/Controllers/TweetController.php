7ee04ccf5996d56e6c71c1e4e4fdf8d2c3d97cb8
jzelAdmin2006 <jo.zeller@protonmail.com>
2023-05-24 16:30:10 +0200

    solve task E2

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -2,14 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\TweetResource;
-use App\Models\Tweet;
 use Illuminate\Http\Request;
 
 class TweetController extends Controller
 {
     public function index()
     {
-        return TweetResource::collection(Tweet::latest()->take(100)->get());
+        return Tweet::with('user')->latest()->take(100)->get();
     }
 }
